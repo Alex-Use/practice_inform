e8e4d49904320cfb50ca9d294d1b257b20bfdcaa
Alex-Use <72986039+Alex-Use@users.noreply.github.com>
2021-02-22 14:55:56 +0300

    all change

--- Smth.c
+++ Smth.c
@@ -1,22 +1,56 @@
 #include <stdio.h>
 
 int main(){
-	int x,y,a,i;
-	printf("Введите число:");
+	int x,y,a,i,f;
+	printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ:");
 	 scanf("%d",&x);
-	y = x;
+	f = x;
 	i = 0;
-	while (x>0){
-		a = x%10;
-		x = x/10;
+	if (x<0){
+		x = abs(x);
+	}
+	y =abs(x);
+	while (x >0){
 		
-		if (i > 0){
-			printf("+ %d *10^ %d",a,i);
+		x= x/10;
+		i= i+1;
+	
+	}
+	int b[i];
+	int k;
+	k=0;
+	 
+	 while(k < i) {
+		a= y%10;
+		y=y/10;
+		b[k] = a;
+	
+		k = k+1;
+		
+	}
+	if (f<0){
+		while (i>0){
+		
+			i=i-1;
+			if (i > 0){
+			printf("- %d *10^ %d ",b[i],i);
 		} 
 		else{
-		printf("%d",a);
+		printf("- %d",b[i]);
 		}
-		 i++;
+	}
+	}
+	else {
+	while (i>0){
+		i=i-1;
+			if (i > 0){
+			printf(" %d *10^ %d +",b[i],i);
+		} 
+		else{
+		printf(" %d",b[i]);
+		}
+		
+}
 	}
 	return 0;
 } 
